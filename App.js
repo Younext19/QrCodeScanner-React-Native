8e2f217209a7b5933624516df4112e6b6f6c92fc
Younext19 <Youneshaddam9@gmail.com>
2020-10-25 09:32:38 +0100

    qr

--- App.js
+++ App.js
@@ -6,25 +6,97 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
+  Linking,
+  FlatList,
   ScrollView,
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
 
 import QRCodeScan from './Components/QrCodeScan';
-const App = () => {
+/*const App = () => {
+  onSuccess = (e) => {
+    console.log(e.data);
+    reactivate();
+  };
   return (
     <View style={{flex: 1}}>
-      <QRCodeScan />
+      <QRCodeScanner
+      showMarker={true}
+        onRead={this.onSuccess}
+        //flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    </View>
+  );
+};*/
+
+const App = () => {
+  const onread = (e) => {
+    console.log(e.data);
+    //Add QR To Flatlist
+
+    //When Added To Flatlist we set count to true
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <QRCodeScanner
+        showMarker={true}
+        onRead={onread}
+        reactivate={true}
+        topContent={
+          <Text style={styles.centerText}>
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
-
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
 export default App;
--- Components/QrCodeScan.js
+++ Components/QrCodeScan.js
@@ -1,58 +1,58 @@
 import React, {Component} from 'react';
 import {
-  Linking,
   Text,
-  TouchableOpacity,
-  Alert,
   View,
-  Modal,
   StyleSheet,
-  Card,
-  Button,
+  Alert,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
-import QRCodeScanner from 'react-native-qrcode-scanner';
-// IMPORTANT TO ADD ON MANIFEST :
-/*<uses-permission android:name="android.permission.VIBRATE"/>
-  <uses-permission android:name="android.permission.CAMERA" />
-<uses-permission android:name="android.permission.RECORD_AUDIO"/>
-  <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
-  <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
-  Library Used : 
-  https://github.com/react-native-camera/react-native-camera*/
-const Cat = () => {
-  // const [count, setCount] = useState(0);
-
-  ifScanDone = (e) => {
-    console.log(e);
+import Camera from 'react-native-camera';
+class QrCodeScan extends Component {
+  onSuccess = (e) => {
+    Linking.openURL(e.data).catch((err) =>
+      console.error('An error occured', err),
+    );
   };
-  return (
-    <QRCodeScanner
-      containerStyle={{backgroundColor: '#FFF', marginBottom: '35%'}}
-      onRead={this.ifScanDone}
-      reactivate={true}
-      permissionDialogMessage="Need Permission To Access Camera"
-      reactivateTimeout={10}
-      showMarker={true}
-      markerStyle={{borderColor: '#fff', borderRadius: 10}}
-      bottomContent={
-        <TouchableOpacity>
-          <Text style={{fontSize: 21, color: 'rgb(0,122,255)'}}></Text>
-        </TouchableOpacity>
-      }
-    />
-  );
-};
+
+  render() {
+    return (
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
     padding: 16,
   },
-  backdrop: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  button: {
-    marginTop: 16,
-  },
 });
-
-export default Cat;
